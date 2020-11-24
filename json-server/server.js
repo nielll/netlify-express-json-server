9cd80aee10dd2c28567968dedac1364b2b33ade4
niel <nico_elmer@niel.site>
2020-11-24 12:34:35 +0100

    setup for use

--- json-server/server.js
+++ json-server/server.js
@@ -1,25 +1,25 @@
-'use strict';
-const express = require('express');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-const bodyParser = require('body-parser');
-var jsonServer = require('json-server');
+const bodyParser = require("body-parser");
+var jsonServer = require("json-server");
 
-app.use('/api', jsonServer.router(path.resolve(__dirname, 'db.json')));
+app.use("/api", jsonServer.router(path.resolve(__dirname, "db.json")));
 
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello from Express.js!</h1>');
-    res.end();
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
+  res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use("/.netlify/functions/server", router); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
